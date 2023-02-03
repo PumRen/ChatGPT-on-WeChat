bece2cab6ac1d3ed272d35f2b869d97316743124
PumRen <124036048+PumRen@users.noreply.github.com>
2023-02-03 12:04:13 +0800

    Update chatgpt.ts
    注释第一个发送

--- src/chatgpt.ts
+++ src/chatgpt.ts
@@ -76,8 +76,8 @@ export class ChatGPTBot {
       console.log(`🎯 Trigger keyword in private chat is: ${this.chatgptTriggerKeyword}`);
       console.log(`🎯 Trigger keyword in group chat is: ${this.chatGroupTriggerKeyword}`);
       // Run an initial test to confirm API works fine
-      await this.onChatGPT("Say Hello World !!!");
-      console.log(`✅ Chatbot starts success, ready to handle message!`);
+      //await this.onChatGPT("Say Hello World !!!");
+      console.log(`✅ Chatbot starts success firstly, ready to handle message!`);
     } catch (e) {
       console.error(`❌ ${e}`);
     }
